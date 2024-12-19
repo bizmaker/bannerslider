1ad20612db087d1a9b7d5f07c04c2a6ebca6c071
bizmaker <124229199+bizmaker@users.noreply.github.com>
2024-12-19 11:37:30 +0900

    Add files via upload

---
+++ app/src/main/java/ss/com/bannerslidersample/MainActivity.java
@@ -0,0 +1,193 @@
+package ss.com.bannerslidersample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+
+import ss.com.bannerslider.Slider;
+import ss.com.bannerslider.indicators.IndicatorShape;
+
+public class MainActivity extends AppCompatActivity {
+    private Slider slider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Slider.init(new PicassoImageLoadingService(this));
+        setupViews();
+    }
+
+    private void setupViews() {
+        setupToolbar();
+        setupPageIndicatorChooser();
+        setupSettingsUi();
+        slider = findViewById(R.id.banner_slider1);
+
+        //delay for testing empty view functionality
+        slider.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                slider.setAdapter(new MainSliderAdapter());
+                slider.setSelectedSlide(0);
+            }
+        }, 1500);
+
+    }
+
+    private void setupSettingsUi() {
+        final SeekBar intervalSeekBar = findViewById(R.id.seekbar_interval);
+        intervalSeekBar.setMax(10000);
+        intervalSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    slider.setInterval(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SeekBar indicatorSizeSeekBar = findViewById(R.id.seekbar_indicator_size);
+        indicatorSizeSeekBar.setMax(getResources().getDimensionPixelSize(R.dimen.max_slider_indicator_size));
+        indicatorSizeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    slider.setIndicatorSize(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SwitchCompat loopSlidesSwitch = findViewById(R.id.checkbox_loop_slides);
+        loopSlidesSwitch.setChecked(true);
+        SwitchCompat mustAnimateIndicators = findViewById(R.id.checkbox_animate_indicators);
+        mustAnimateIndicators.setChecked(true);
+
+        loopSlidesSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                slider.setLoopSlides(b);
+            }
+        });
+
+        mustAnimateIndicators.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                slider.setAnimateIndicators(b);
+            }
+        });
+
+        SwitchCompat hideIndicatorsSwitch = findViewById(R.id.checkbox_hide_indicators);
+        hideIndicatorsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    slider.hideIndicators();
+                } else {
+                    slider.showIndicators();
+                }
+            }
+        });
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView githubSourceImageView = findViewById(R.id.image_github);
+        githubSourceImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/saeedsh92/Banner-Slider");
+
+                if (Build.VERSION.SDK_INT > 15) {
+                    CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
+                    intentBuilder.setToolbarColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimary));
+                    intentBuilder.setSecondaryToolbarColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimaryDark));
+                    intentBuilder.setStartAnimations(MainActivity.this, android.R.anim.fade_in, android.R.anim.fade_out);
+                    intentBuilder.setExitAnimations(MainActivity.this, android.R.anim.fade_in, android.R.anim.fade_out);
+                    CustomTabsIntent customTabsIntent = intentBuilder.build();
+                    customTabsIntent.launchUrl(MainActivity.this, uri);
+                } else {
+                    startActivity(Intent.createChooser(new Intent(Intent.ACTION_VIEW, uri), "Choose Browser..."));
+                }
+
+            }
+        });
+    }
+
+
+    private void setupPageIndicatorChooser() {
+
+        String[] pageIndicatorsLabels = getResources().getStringArray(R.array.page_indicators);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                pageIndicatorsLabels
+        );
+        Spinner spinner = findViewById(R.id.spinner_page_indicator);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        slider.setIndicatorStyle(IndicatorShape.CIRCLE);
+                        break;
+                    case 1:
+                        slider.setIndicatorStyle(IndicatorShape.DASH);
+                        break;
+                    case 2:
+                        slider.setIndicatorStyle(IndicatorShape.ROUND_SQUARE);
+                        break;
+                    case 3:
+                        slider.setIndicatorStyle(IndicatorShape.SQUARE);
+                        break;
+                    case 4:
+                        slider.setSelectedSlideIndicator(ContextCompat.getDrawable(MainActivity.this, R.drawable.selected_slide_indicator));
+                        slider.setUnSelectedSlideIndicator(ContextCompat.getDrawable(MainActivity.this, R.drawable.unselected_slide_indicator));
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
